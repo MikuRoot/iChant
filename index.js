4b368a386d131cebdd73744d0ae072749936d37e
MikuRoot <tranthevu.iuh@gmail.com>
2022-03-05 23:55:25 +0700

    mikuroot commit main => configured reactotron

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
  * @format
  */
 
+if(__DEV__) {
+    import('./app/ReactotronConfig').then(() => console.log('Reactotron Configured'))
+}
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
