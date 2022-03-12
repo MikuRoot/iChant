e58ad8c21259a179a7045eaa2742b510c5c4f41e
MikuRoot <tranthevu.iuh@gmail.com>
2022-03-13 00:18:10 +0700

    mikuroot commit main => Added 3 more images and created an index file for exporting images

---
+++ app/assets/images/index.js
@@ -0,0 +1,33 @@
+import archangel from './archangel.jpg';
+import * as namSuMung from './5SuMung.jpg';
+import * as namSuSang from './5SuSang.jpg';
+import * as namSuThuong from './5SuThuong.jpg';
+import * as namSuVui from './5SuVui.jpg';
+import * as dangThanhGia from './14dangThanhGia.jpg';
+import angel from './angel.png';
+import angel2 from './angel2.png';
+import catholicGrave from './catholicGrave.jpg';
+import Gabriel from './Gabriel.jpg';
+import Michael from './Michael.jpg';
+import Raphael from './Schiffer-RAPHAEL.jpg';
+import sacredHeartOfJesus from './sacredHeartOfJesus.jpg';
+import sacredHeartOfJesus2 from './sacredHeartOfJesus2.jpg';
+import holyCross from './theHolyCross.jpeg';
+
+export default {
+    archangel,
+    namSuMung,
+    namSuSang,
+    namSuThuong,
+    namSuVui,
+    dangThanhGia,
+    angel,
+    angel2,
+    catholicGrave,
+    Gabriel,
+    Michael,
+    Raphael,
+    sacredHeartOfJesus,
+    sacredHeartOfJesus2,
+    holyCross
+}
